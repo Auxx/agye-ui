8d6492a184850e75fb29839495c678ae305556b3
Aleksandr 'Aux' Ivanov <aux@hexmode.org>
2025-06-09 23:19:04 +0100

    Raised button stub

--- libs/@hexmode/agye/src/index.ts
+++ libs/@hexmode/agye/src/index.ts
@@ -1,1 +1,3 @@
 export * from './lib/agye/agye.component';
+
+export * from './lib/buttons/raised-button/raised-button.component';
---
+++ libs/@hexmode/agye/src/lib/buttons/raised-button/raised-button.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'agye-raised-button',
+  imports: [CommonModule],
+  templateUrl: './raised-button.component.html',
+  styleUrl: './raised-button.component.scss',
+})
+export class RaisedButtonComponent {}
