6c2cb08eb2b674b4bd013d70199dc06ac19e02b8
Aleksandr 'Aux' Ivanov <aux@hexmode.org>
2025-06-10 11:45:32 +0100

    Finished docs

--- libs/@hexmode/agye/src/lib/buttons/raised-button/raised-button.component.stories.ts
+++ libs/@hexmode/agye/src/lib/buttons/raised-button/raised-button.component.stories.ts
@@ -3,6 +3,11 @@ import { Projection } from '../../types';
 import { allButtonTypes, allColors } from '../types';
 import { RaisedButtonComponent } from './raised-button.component';
 
+/**
+ * A basic button which can render any valid HTML as its label.
+ * It comes with predefined transitions and animations as well as the ability to customise its look and feel.
+ * `RaisedButton` also supports custom styling, see below.
+ */
 const meta: Meta<Projection<RaisedButtonComponent>> = {
   component: RaisedButtonComponent,
   title: 'Components/Buttons/RaisedButton',
@@ -44,23 +49,13 @@ const meta: Meta<Projection<RaisedButtonComponent>> = {
       description: 'Flag to disable the button',
       table: { category: 'Behaviour', defaultValue: { summary: 'false' } }
     }
-  },
-
-  parameters: {
-    docs: {
-      description: {
-        component: `
-A basic button which can render any valid HTML as its label.
-        `
-      }
-    }
   }
 };
 export default meta;
 type Story = StoryObj<Projection<RaisedButtonComponent>>;
 
 export const Primary: Story = {
-  // tags: [ '!autodocs' ],
+  tags: [ '!autodocs' ],
   render: props => ({
     props,
     template: `
@@ -69,6 +64,63 @@ export const Primary: Story = {
       [disabled]="disabled"
       [color]="color">
       {{ ngContent }}
+    </agye-raised-button>
+    `
+  })
+};
+
+export const SimplePrimaryButton: Story = {
+  render: props => ({
+    props,
+    template: `
+    <agye-raised-button>Apply</agye-raised-button>
+    `
+  })
+};
+
+export const DeleteButton: Story = {
+  render: props => ({
+    props,
+    template: `
+    <agye-raised-button color="warn">Delete</agye-raised-button>
+    `
+  })
+};
+
+/**
+ * It is possible to customise most visual aspects of `RaisedButton`
+ * with the help of CSS variables. CSS variables can be set either through
+ * a stylesheet or with the help of Angular `[style]` binding as seen in the example below.
+ * The `[style]` binding can also accept a single object with multiple variables.
+ *
+ * The following CSS variables can be used for custom styling:
+ *
+ * | Variable                           |
+ * |------------------------------------|
+ * | --raised-button-font-family        |
+ * | --raised-button-font-weight        |
+ * | --raised-button-font-size          |
+ * | --raised-button-font-style         |
+ * | --raised-button-border-radius      |
+ * | --raised-button-color              |
+ * | --raised-button-hover-color        |
+ * | --raised-button-disabled-color     |
+ * | --raised-button-background-color   |
+ * | --raised-button-vertical-padding   |
+ * | --raised-button-horizontal-padding |
+ * | --raised-button-height             |
+ * | --raised-button-gap                |
+ */
+export const CustomStyling: Story = {
+  render: props => ({
+    props,
+    template: `
+    <agye-raised-button
+      [style.--raised-button-color]="'#FEE'"
+      [style.--raised-button-background-color]="'#F30'"
+      [style.--raised-button-hover-color]="'#FFF'"
+      [style.--raised-button-border-radius]="'0'">
+      Remove option
     </agye-raised-button>
     `
   })
